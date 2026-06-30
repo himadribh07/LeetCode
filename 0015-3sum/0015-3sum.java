40d05fc28e5547222d5f8349400168d2556599fa
Himadri Bhattacharya <62461681+himadribh07@users.noreply.github.com>
2026-06-30 18:37:59 +0530

    Time: 28 ms (96.98%), Space: 59.1 MB (59.71%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,24 +1,36 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // List<List<Integer>> ans = new ArrayList<>();
-        int n = nums.length;
-        Set<List<Integer>> set = new HashSet<>();
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
 
-        for(int i=0;i<n;i++){
-            Map<Integer,Integer> map = new HashMap<>();
-            for(int j=i+1;j<n;j++){
-                int diff = -(nums[i]+nums[j]);
-                if(map.containsKey(diff)){
-                    List<Integer> temp= Arrays.asList(diff, nums[i], nums[j]);
-                    Collections.sort(temp);
-                    set.add(temp);
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
+            
+            int j = i+1, k = nums.length-1;
+
+            while(j<k){
+
+                int sum = nums[i]+nums[j]+nums[k];
+
+                if(sum>0)
+                    k--;
+                else if(sum<0)
+                    j++;
+                else{
+
+                    ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1])
+                        j++;
+                    while(j<k && nums[k]==nums[k+1])
+                        k--;
+
                 }
-
-                map.put(nums[j],j);
-
-            } 
+            }
         }
 
-        return new ArrayList<>(set);
+        return ans;
     }
 }
